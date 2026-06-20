f4f6363187ce7da762bc9645a3dd58173d2b7015
Bartosz Szymankiewicz <wrx94039@student.wroclaw.merito.pl>
2026-06-20 18:56:09 +0200

    Dodano plik z CLI

---
+++ src/main.py
@@ -0,0 +1,36 @@
+import argparse
+from logic.logic import Calculator
+
+
+def main():
+    calc = Calculator()
+
+    parser = argparse.ArgumentParser(description="Simple calculator CLI")
+    parser.add_argument("a", type=float)
+    parser.add_argument("b", type=float)
+    parser.add_argument(
+        "--op",
+        choices=["add", "sub", "mul", "div"],
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    try:
+        if args.op == "add":
+            result = calc.add(args.a, args.b)
+        elif args.op == "sub":
+            result = calc.subtract(args.a, args.b)
+        elif args.op == "mul":
+            result = calc.multiply(args.a, args.b)
+        elif args.op == "div":
+            result = calc.divide(args.a, args.b)
+
+        print(result)
+
+    except ValueError as e:
+        print(f"Błąd: {e}")
+
+
+if __name__ == "__main__":
+    main()
